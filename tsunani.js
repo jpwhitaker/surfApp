178fd3ff391614ff96b5a969acf26dba35e16eb0
jpwhitaker <jpw@hawaii.edu>
2013-01-15 20:28:18 -0800

    able to add users, saving username and profile data to mongo db

--- tsunani.js
+++ tsunani.js
@@ -6,7 +6,21 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       var phoneNumber = document.getElementById('phoneNumber').value;
       var password = document.getElementById('password').value
-        Accounts.createUser({'username':phoneNumber, 'password':password})
+        Accounts.createUser({
+        'username':phoneNumber,
+        'password':password,
+        'profile' : {
+          'notifyHeight' : [3,5],
+          'notifyShores' : ['n','s','e','w'],
+          'notifyTime'   : 0600,
+          'notifyText'   : true,
+          'notifyEmail'  : false,
+          'email'        : '',
+          'notifications': true
+        },
+
+
+       })
         console.log(phoneNumber)
         console.log(password);
         ;
