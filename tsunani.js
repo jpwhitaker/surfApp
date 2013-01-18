025c8134ae0fdc3cfd0a76fe7aa980a41438b9e6
jpwhitaker <jpw@hawaii.edu>
2013-01-18 11:09:39 -0800

    sending texts on registering user to user name, changed pass to username

--- tsunani.js
+++ tsunani.js
@@ -5,10 +5,9 @@ if (Meteor.isClient){
       // template data, if any, is available in 'this'
 
       var phoneNumber = document.getElementById('phoneNumber').value;
-      var password = document.getElementById('password').value
         Accounts.createUser({
         'username':phoneNumber,
-        'password':password,
+        'password':phoneNumber,
         'profile' : {
           'notifyHeight' : [3,5],
           'notifyShores' : ['n','s','e','w'],
@@ -27,19 +26,20 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
 
     var verifyNumber = function(user){
+      //SMS Meteor!!
+      // Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC247cdc55daacf786ddd9c6ea0e51534f/SMS/Messages.json',
+      // {
+      //   params:{From:'+12408216248', To:user.username, Body: 'Reply with SURF to verify your account!'},
+      //   auth: 'AC247cdc55daacf786ddd9c6ea0e51534f:b08d5f3880e3e3783e565d978031f72d',
+      //   headers: {'content-type':'application/x-www-form-urlencoded'}
+      // }, function () {
+      //     console.log(arguments, 'Log')
+      //   }
+      // );
+      console.log(user.username)
 
-      Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC247cdc55daacf786ddd9c6ea0e51534f/SMS/Messages.json',
-      {
-        params:{From:'+12408216248', To:'+18082256917', Body: user.name},
-        auth: 'AC247cdc55daacf786ddd9c6ea0e51534f:b08d5f3880e3e3783e565d978031f72d',
-        headers: {'content-type':'application/x-www-form-urlencoded'}
-      }, function () {
-          console.log(arguments, 'Log')
-        }
-      );
     }
-    // code to run on server at startup
-    // console.log(Meteor.users.find({}) );
+  
 
   Accounts.onCreateUser(function(options,user){
     verifyNumber(user);
