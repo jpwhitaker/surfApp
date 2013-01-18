b5f9ef3c07fc29c9293b1fb986769491a060931d
jpwhitaker <jpw@hawaii.edu>
2013-01-18 10:10:19 -0800

    texting working, had to change data to params

--- tsunani.js
+++ tsunani.js
@@ -1,5 +1,4 @@
-if (Meteor.isClient) {
-
+if (Meteor.isClient){
 
   Template.signUp.events({
     'click #submit' : function () {
@@ -22,13 +21,32 @@ if (Meteor.isClient) {
       })
     }
   });
-
-
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+
+    var verifyNumber = function(user){
+
+      Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC247cdc55daacf786ddd9c6ea0e51534f/SMS/Messages.json',
+      {
+        params:{From:'+12408216248', To:'+18082256917', Body: user.name},
+        auth: 'AC247cdc55daacf786ddd9c6ea0e51534f:b08d5f3880e3e3783e565d978031f72d',
+        headers: {'content-type':'application/x-www-form-urlencoded'}
+      }, function () {
+          console.log(arguments, 'Log')
+        }
+      );
+    }
     // code to run on server at startup
     // console.log(Meteor.users.find({}) );
+
+  Accounts.onCreateUser(function(options,user){
+    verifyNumber(user);
+    user.password = options.password;
+      if (options.profile)
+        user.profile = options.profile;
+      return user;
+    })
   });
 }
