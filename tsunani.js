5b70113661a5b4b40ad416f7699ac8b4ba53e242
jpwhitaker <jpw@hawaii.edu>
2013-01-19 10:42:40 -0800

    TEXTS A PIN, STORES PIN IN USER PROFILE

--- tsunani.js
+++ tsunani.js
@@ -5,10 +5,13 @@ if (Meteor.isClient){
       // template data, if any, is available in 'this'
 
       var phoneNumber = document.getElementById('phoneNumber').value;
+      var verifyPin = Math.floor(Math.random()*9000)+1000;
         Accounts.createUser({
         'username':phoneNumber,
         'password':phoneNumber,
         'profile' : {
+          'verifyPin'    : verifyPin,
+          'verified'     : false,
           'notifyHeight' : [3,5],
           'notifyShores' : ['n','s','e','w'],
           'notifyTime'   : 600,
@@ -17,7 +20,8 @@ if (Meteor.isClient){
           'email'        : '',
           'notifications': true
         },
-      })
+
+      }, function(error){console.log(error)})
     }
   });
 }
@@ -25,24 +29,25 @@ if (Meteor.isClient){
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
-    var verifyNumber = function(user){
+    var verifyNumber = function(user, options){
       //SMS Meteor!!
-      // Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC247cdc55daacf786ddd9c6ea0e51534f/SMS/Messages.json',
-      // {
-      //   params:{From:'+12408216248', To:user.username, Body: 'Reply with SURF to verify your account!'},
-      //   auth: 'AC247cdc55daacf786ddd9c6ea0e51534f:b08d5f3880e3e3783e565d978031f72d',
-      //   headers: {'content-type':'application/x-www-form-urlencoded'}
-      // }, function () {
-      //     console.log(arguments, 'Log')
-      //   }
-      // );
+      Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC247cdc55daacf786ddd9c6ea0e51534f/SMS/Messages.json',
+      {
+        params:{From:'+12408216248', To:user.username, Body: 'Verify your account by entering the following: ' + options.profile.verifyPin},
+        auth: 'AC247cdc55daacf786ddd9c6ea0e51534f:b08d5f3880e3e3783e565d978031f72d',
+        headers: {'content-type':'application/x-www-form-urlencoded'}
+      }, function () {
+          console.log(arguments, 'Log')
+        }
+      );
       console.log(user.username)
 
     }
-  
+
 
   Accounts.onCreateUser(function(options,user){
-    verifyNumber(user);
+    console.log(options, user)
+    verifyNumber(user, options);
     user.password = options.password;
       if (options.profile)
         user.profile = options.profile;
