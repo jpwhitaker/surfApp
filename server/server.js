72d699000abde61ded8377a4430237716d5e2c46
jpwhitaker <jpw@hawaii.edu>
2013-01-19 12:29:52 -0800

    moved files to client, server - working on user pages now

---
+++ server/server.js
@@ -0,0 +1,26 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    var verifyNumber = function(user, options){
+      //SMS Meteor!!
+      Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC247cdc55daacf786ddd9c6ea0e51534f/SMS/Messages.json',
+      {
+        params:{From:'+12408216248', To:user.username+'take out to text', Body: 'Thanks for signing up with Tsunani.com!  Verify your account by entering the following: ' + options.profile.verifyPin},
+        auth: 'AC247cdc55daacf786ddd9c6ea0e51534f:b08d5f3880e3e3783e565d978031f72d',
+        headers: {'content-type':'application/x-www-form-urlencoded'}
+      }, function () {
+          console.log(arguments, 'Log')
+        }
+      );
+    }
+    Accounts.onCreateUser(function(options,user){
+      console.log(options, user)
+      verifyNumber(user, options);
+      user.password = options.password;
+      if (options.profile){
+        user.profile = options.profile;
+        return user;
+      } 
+    })
+  })
+}
