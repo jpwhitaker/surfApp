faf53f0771d294c42ed257f7c20d3bfd534af626
jpwhitaker <jpw@hawaii.edu>
2013-01-20 20:50:33 -0800

    getting xml, doing basic parsing

--- server/scraper.js
+++ server/scraper.js
@@ -2,10 +2,15 @@ Meteor.methods({scrapeData:scrapeData})
 
 function scrapeData(abc) {
   Meteor.http.call("GET", 'http://www.prh.noaa.gov/hnl/xml/Surf.xml', function (err,res){
-    console.log(res.content)
+    parseData(res.content)
 
   });
 }
 
 setInterval(scrapeData(),3600000);
 
+
+function parseData(xml) {
+var surf = xml.match((/\d{1,2}( to )\d{1,2}/g))
+  console.log(surf)
+}
