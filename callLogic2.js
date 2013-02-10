9ac1ed12dca458a0a907cb9a297be010a5a732bd
jpwhitaker <jpw@hawaii.edu>
2013-02-09 21:59:51 -0800

    call logic working for everything but tomorrow's heights WIP

--- callLogic2.js
+++ callLogic2.js
@@ -5,7 +5,7 @@ var sendSurfReport= function(){
 }
 
 var checkUser = function(){
-  
+
   var currentHour = moment.utc().subtract('hours',10).format('h')
   var currentMinute = moment.utc().subtract('hours',10).format('m')
   var currentDay = moment.utc().subtract('hours',10).format('dddd');
@@ -84,17 +84,38 @@ var checkUser = function(){
     }
   })
 
+  //check if the wave heights (today or tomorrow) match their settings for each shore
+  var heights = verifiedData.findOne({today:{$exists:true}}, {sort:{today:-1}})
+  var tomorrowsHeights = verifiedData.findOne({tomorrow:{$exists:true}}, {sort:{tomorrow:-1}})
+
+  var matchWaveSettings = _.filter(notificationsOn, function(user){
+    var shore = user.profile.notifyShores;
+    var userHeight = user.profile.notifyHeight;
+
+    if(shore.north === true && userHeight[0] >= heights.north.min && userHeight[1]<= heights.north.max){
+      console.log('called');
+    } else if (shore.south === true && userHeight[0] >= heights.south.min && userHeight[1]<= heights.south.max){
+      console.log('called');
+    } else if (shore.east === true && userHeight[0] >= heights.east.min && userHeight[1]<= heights.east.max){
+      console.log('called');
+    } else if (shore.west === true && userHeight[0] >= heights.west.min && userHeight[1]<= heights.west.max){
+      console.log('called');
+    }
+
+  });
 
 
-  
-  
+
+
+  // matchWaveSettings();
+
   var callUser = function(){
 
     _.each(notificationsOn, function(user){
 
       console.log(user.username);
       console.log(matchingTimes);
-      console.log(notificationsOn)
+      console.log(notificationsOn);
       Meteor.call('sendText', user + 'remove to send text','Surfs up!  North is 10, 15 ft today!')
     })
   }
